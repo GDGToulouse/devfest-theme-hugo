f41cb36867044e21e37786737cdc621eb2bbecfd
Igor Laborie <ilaborie@gmail.com>
2019-01-23 08:40:35 +0100

    fix responive

--- src/script/background.js
+++ src/script/background.js
@@ -1,10 +1,10 @@
 import './konami';
 
-const elt = document.querySelector('.home .jumbo');
-if (elt) {
-    const i = 1 + (Math.random() * 4 | 0);
-    elt.style.setProperty('--bg-img', `url(/images/backgrounds/back-${i}.jpg)`);
-}
+// const elt = document.querySelector('.home .jumbo');
+// if (elt) {
+//     const i = (Math.random() * 5) | 0;
+//     elt.style.setProperty('--bg-img', `url(/images/backgrounds/back-${i}.jpg)`);
+// }
 
 new Konami(function () {
     const elt = document.querySelector('.jumbo');
