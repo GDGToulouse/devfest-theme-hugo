6a8d7c8af563c5fa71e36e31fe6425ce85702f36
Igor Laborie <ilaborie@gmail.com>
2019-08-24 18:56:35 +0200

    update tools

--- icons.js
+++ icons.js
@@ -1,21 +1,25 @@
 const {readFileSync, writeFileSync} = require('fs');
 const {sync: glob} = require('glob');
-const {Logger} = require('plop-logger');
+const {Logger, LogLevel} = require('plop-logger');
 const {colorEmojiConfig} = require('plop-logger/lib/extra/colorEmojiConfig');
 
 Logger.config = colorEmojiConfig;
 const logger = Logger.getLogger('icons');
+logger.level = LogLevel.All;
 
-const data = glob(`src/icons/*.svg`)
-    .map(file => {
-            const parts = file.split('/');
-            const id = parts[parts.length - 1].split('.')[0];
-            return readFileSync(file, 'utf8')
-                .replace(`<svg xmlns="http://www.w3.org/2000/svg"`, `\t<symbol id="${id}"`)
-                .replace(`</svg>`, `\t</symbol>`);
-        }
-    );
+let svgFiles = `src/icons/*.svg`;
+logger.info('SVG aggregation of', svgFiles);
 
+const data = glob(svgFiles)
+  .map(file => {
+      const parts = file.split('/');
+      const id = parts[parts.length - 1].split('.')[0];
+      logger.debug('handle', id);
+      return readFileSync(file, 'utf8')
+        .replace(`<svg xmlns="http://www.w3.org/2000/svg"`, `\t<symbol id="${id}"`)
+        .replace(`</svg>`, `\t</symbol>`);
+    }
+  );
 
 const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" class="visually-hidden">
   ${data.join('\n')}
--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,30 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+const {Logger, LogLevel} = require('plop-logger');
+const {colorEmojiConfig} = require('plop-logger/lib/extra/colorEmojiConfig');
+
+Logger.config = colorEmojiConfig;
+const logger = Logger.getLogger('icons');
+logger.level = LogLevel.All;
 
 const globals = {firebase: 'firebase'};
 
 export default {
-    input: 'src/script/main.js',
-    output: [
-        {file: 'static/theme.js', format: 'esm', globals}
-    ],
-    plugins: [
-        resolve(),
-        babel({
-            exclude: 'node_modules/**' // only transpile our source code
-        })
-    ],
-    external: [
-        'firebase',
-        'firebase/firestore'
-    ]
+  input: 'src/script/main.js',
+  output: [
+    {file: 'static/theme.js', format: 'esm', globals}
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ],
+  external: [
+    'firebase',
+    'firebase/firestore'
+  ],
+  onwarn(warning) {
+    logger.warn(warning.message, warning);
+  }
 }
